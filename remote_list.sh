2502777a584df7507269e5ad84c10cb4e095c36c
Kyle Luzny <kluzny@gmail.com>
2020-08-09 07:35:54 -0500

    added a local backup list

--- remote_list.sh
+++ remote_list.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export BORG_REPO="$(jq -r '.local.repo' .secrets.json)"
+export BORG_RSH="$(jq -r '.remote.rsh' .secrets.json )"
+export BORG_REPO="$(jq -r '.remote.repo' .secrets.json)"
 export BORG_PASSPHRASE="$(jq -r '.password' .secrets.json)"
 
 info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
